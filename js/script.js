03a87d16fd6fa2952ddb4fd0bb7494ce7c2de450
yetimeh <senukabandara2006@gmail.com>
2024-11-07 19:10:08 +0530

    Create particles in header area | Create typewriter subtext

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+typewrite();
+
+particlesJS.load('particles-js', 'assets/snow.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+function typewrite() {
+
+    var typed = new Typed('#subText', {
+      strings:[
+
+        "I'm a software engineer... and a full-stack magician.",
+        "I bridge the gap between design & database.",
+        "Software developer by night, student by day.",
+        "Java enthusiast, always learning.",
+        "I specialize in a variety of fields!",
+        "I write code... and sometimes it even works!",
+        "Delivering perfect software is my thing.",
+        "Building things is my passion."
+
+      ],
+      typeSpeed: 68,
+      smartBackspace: true,
+      shuffle: true,
+      loop: true,
+      backSpeed: 60,
+
+    });
+
+
+    
+}       
